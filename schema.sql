cb2a26721ab793a612374194339a46c4817ce871
Nekomer <l.a.b.tssj4@gmail.com>
2024-04-15 09:24:36 -0600

    fixes

--- schema.sql
+++ schema.sql
@@ -13,10 +13,18 @@ CREATE TABLE IF NOT EXISTS travel_tickets (
     destination_longitud double precision
 );
 
-DO $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'travel_tickets') THEN
-        COPY travel_tickets FROM '/app/challenge_dataset.csv' DELIMITER ',' CSV HEADER
-        ALTER TABLE travel_tickets ADD COLUMN id SERIAL PRIMARY KEY;
+IF EXISTS (
+        SELECT 1 
+        FROM information_schema.tables 
+        WHERE table_name = 'travel_tickets'
+    ) THEN
+        IF (SELECT count(*) FROM travel_tickets) = 0 THEN
+            COPY travel_tickets FROM '/app/challenge_dataset.csv' DELIMITER ',' CSV HEADER;
+            ALTER TABLE travel_tickets ADD COLUMN id SERIAL PRIMARY KEY;
+        ELSE
+            RAISE NOTICE 'La tabla existe pero no está vacía';
+        END IF;
+    ELSE
+        RAISE NOTICE 'La tabla no existe';
     END IF;
 END $$;
